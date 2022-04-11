a394ebb394f9b79ca47aaca0ac04952aa45875b2
Thanh NT <thanh.nguyen2612@hcmut.edu.vn>
2022-04-11 22:41:10 +0700

    Fix toggle

--- Assets/Scripts/ToggleSwitch.cs
+++ Assets/Scripts/ToggleSwitch.cs
@@ -10,10 +10,10 @@ public class ToggleSwitch : MonoBehaviour
     public Toggle toggle;
     public RectTransform toggleIndicator;
     public Image backgroundImage;
-    public Color onColor;
-    public Color offColor;
     public float tweenTime = 0.25f;
 
+    private Color onColor;
+    private Color offColor;
     private float onX;
     private float offX;
 
@@ -21,9 +21,11 @@ public class ToggleSwitch : MonoBehaviour
     {
         offX = toggleIndicator.anchoredPosition.x;
         onX = -offX;
+        onColor = new Color(0.12f, 1f, 0f, 1f);
+        offColor = backgroundImage.color;
         toggle.onValueChanged.AddListener(OnSwitch);
 
-        if (toggle.isOn) OnSwitch(true);
+        OnSwitch(toggle.isOn);
     }
 
     void OnSwitch(bool on) {
@@ -32,7 +34,7 @@ public class ToggleSwitch : MonoBehaviour
     }
 
     private void ToggleColor(bool value) {
-        backgroundImage.color = value ? onColor : offColor;
+        backgroundImage.DOColor(value ? onColor : offColor, tweenTime);
     }
 
     private void MoveIndicator(bool value) {
